4f28559db02ec2a21effdf35489baa410dfa51c7
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2020-08-15 20:38:11 +0600

    dfsdfs

--- backend/settings.py
+++ backend/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILE_DIRS = [
-    os.path.join(BASE_DIR, "static/images"),
-    os.path.join(BASE_DIR, "static/css"),
-    os.path.join(BASE_DIR, "static"),
+    "static/images",
+    "static/css",
+    "staticfiles",
 ]
 
 MEDIA_URL = '/media/'
