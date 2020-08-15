ccab5fcb608a29e4c55edabb461de486adde7d79
Ataf Fazledin <1705066@ugrad.cse.buet.ac.bd>
2020-08-15 19:40:23 +0600

    asdas

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mainsite',
-    'rest_framework',
+    #'rest_framework',
     #'cotter',
     #'api',
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('',        include('mainsite.urls')),
     #path('api/',     include('api.urls')),
     #path('cotter/',  include('cotter.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
